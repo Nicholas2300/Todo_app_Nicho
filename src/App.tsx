ed859f8b9c4412d9c151d7937906a45806368a2c
DESKTOP-Q25UO2A\ASUS <gervasiusnicholas@gmail.com>
2021-11-10 15:22:44 +0700

    refactor: rename some variable & feat: can delete task

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,33 @@ const App: FC = () => {
   /*Untuk vairable input agar bisa di input */
   const [todoName, setTodoName] = useState<string>("");
   /*Untuk melihat hasil inputan */
-  const [todoNameList, viewTodoNameList] = useState<ItodoList[]>([]);
+  const [todoNameList, setTodoNameList] = useState<ItodoList[]>([]);
 
-  const input_todo_task = (event: ChangeEvent<HTMLInputElement>): void =>{
+  const inputTodoTask = (event: ChangeEvent<HTMLInputElement>): void =>{
     setTodoName(event.target.value);
   };
 
   const addTodoList = (): void =>{
     const newTodo = {todoListName: todoName};
-    viewTodoNameList([...todoNameList, newTodo]);
+    setTodoNameList([...todoNameList, newTodo]);
     setTodoName("");
-  }
+  };
+
+  const completeTodoList = (todoListToDelete: string): void =>{
+    setTodoNameList(todoNameList.filter((todo) =>{
+      return todo.todoListName !== todoListToDelete
+    }))
+  };
 
   return (
     <div className="App">
-      <div className="Todo_List_Input">
-        <input type="text" placeholder="Insert your task here" value={todoName} onChange={input_todo_task}/>
+      <div className="TodoListInput">
+        <input type="text" placeholder="Insert your task here" value={todoName} onChange={inputTodoTask}/>
         <button onClick={addTodoList}>Add</button>
       </div>
-      <div className="Todo_List">
+      <div className="TodoList">
         {todoNameList.map((todo: ItodoList, key: number) =>{
-          return <ViewTodoList key={key} todo={todo}/>
+          return <ViewTodoList key={key} todo={todo} completeTodoList={completeTodoList}/>
         })}
       </div>
     </div>
--- src/Component/ViewTodoList.tsx
+++ src/Component/ViewTodoList.tsx
@@ -3,12 +3,17 @@ import { ItodoList } from '../Interface';
 
 interface Props{
     todo: ItodoList;
+    completeTodoList(todoListToDelete: string): void;
 }
 
-const ViewTodoList = ({todo}: Props) =>{
+const ViewTodoList = ({todo, completeTodoList}: Props) =>{
     return(
-        <div>
-            {todo.todoListName}
+        <div className="todoList">
+            <div className="content">
+                <span>{todo.todoListName}</span>
+            </div>
+            <button>Edit</button>
+            <button onClick={() =>{completeTodoList(todo.todoListName)}}>X</button>
         </div>
     )
 };
